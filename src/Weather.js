19b51260c18b50a45bb11bf9ce4290dfcb042b38
AndreiaIsa <andreiagoncalvesrh@gmail.com>
2022-11-08 18:41:49 +0000

    created weather info component to activate search

--- src/Weather.js
+++ src/Weather.js
@@ -2,27 +2,43 @@ import React, { useState } from "react";
 import axios from "axios";
 
 import "./Weather.css";
+import WeatherInfo from "./WeatherInfo";
 
 export default function Weather(props) {
+  const [city, setCity] = useState(props.defaultCity);
   const [weatherData, setWeatherData] = useState({ ready: false });
 
-  function handleSubmit(response) {
+  function handleResponse(response) {
     setWeatherData({
       ready: true,
       temperature: Math.round(response.data.main.temp),
       wind: Math.round(response.data.wind.speed),
       humidity: response.data.main.humidity,
       description: response.data.weather[0].description,
+      date: new Date(response.data.dt * 1000),
       city: response.data.name,
-      date: "Sunday 6 November 2022",
-      time: "21:00",
     });
+  }
+
+  function search() {
+    const apiKey = "2a49b45425dfa7f4d04978f73b59d1fd";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(handleResponse);
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    search();
+  }
+
+  function handleCityChange(event) {
+    setCity(event.target.value);
   }
 
   if (weatherData.ready) {
     return (
       <div className="Weather">
-        <form id="search-form">
+        <form onSubmit={handleSubmit}>
           <span className="search-form">
             <div className="mb-5">
               <input
@@ -31,105 +47,13 @@ export default function Weather(props) {
                 className="form-control search-field"
                 placeholder="Search City"
                 autoComplete="off"
+                onChange={handleCityChange}
               />
             </div>
           </span>
         </form>
 
-        <section className="actual-location">
-          <div className="row">
-            <div className="col-12">
-              <h1>
-                <span className="number-degrees">
-                  {weatherData.temperature}
-                </span>
-                <span className="celcius">
-                  <a href="/" class="active">
-                    °C
-                  </a>
-                </span>
-                <span className="fahrenheit">
-                  <a href="/" className="active">
-                    °F
-                  </a>
-                </span>
-                <span className="actual-weather">🌥️</span>
-              </h1>
-            </div>
-            <div className="col"></div>
-          </div>
-          <div className="row">
-            <span className="city">{weatherData.city}</span>
-            <span className="current-location">
-              <button>🧭</button>
-            </span>
-          </div>
-          <div className="row">
-            <h2 id="date">{weatherData.date}</h2>
-          </div>
-          <div className="row">
-            <h3>{weatherData.time}</h3>
-          </div>
-
-          <ul>
-            <li>
-              <span className="hum">Humidity: </span>
-              <span className="humid">{weatherData.humidity}</span>
-              <span className="percentage">%</span>
-            </li>
-            <li>
-              <span className="wind-speed">Wind: </span>
-              <span className="wind">{weatherData.wind}</span>
-              <span className="km"> Km/h</span>
-            </li>
-            <li className="text-capitalize">
-              <span className="weather-description">
-                {weatherData.description}
-              </span>
-            </li>
-          </ul>
-        </section>
-
-        <section className="next-days">
-          <span className="weather-5days">
-            <div className="row">
-              <div className="col-2">🌥️</div>
-              <div className="col-2">🌥️</div>
-              <div className="col-2">🌥️</div>
-              <div className="col-2">🌥️</div>
-              <div className="col-2">🌦️</div>
-              <div className="col-2">🌦️</div>
-            </div>
-          </span>
-
-          <span className="week-days">
-            <div className="row">
-              <div className="col-2">Sunday</div>
-              <div className="col-2">Monday</div>
-              <div className="col-2">Tuesday</div>
-              <div className="col-2">Wednesday</div>
-              <div className="col-2">Thursday</div>
-              <div className="col-2">Friday</div>
-            </div>
-          </span>
-
-          <span className="daily-temperatures">
-            <div className="row">
-              <div className="col-1">26°</div>
-              <div className="col-1">21°</div>
-              <div className="col-1">24°</div>
-              <div className="col-1">20°</div>
-              <div className="col-1">24°</div>
-              <div className="col-1">20°</div>
-              <div className="col-1">23°</div>
-              <div className="col-1">20°</div>
-              <div className="col-1">25°</div>
-              <div className="col-1">20°</div>
-              <div className="col-1">20°</div>
-              <div className="col-1">24°</div>
-            </div>
-          </span>
-        </section>
+        <WeatherInfo data={weatherData} />
 
         <footer>
           <a
@@ -144,9 +68,7 @@ export default function Weather(props) {
       </div>
     );
   } else {
-    const apiKey = "2a49b45425dfa7f4d04978f73b59d1fd";
-    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${props.defaultCity}&appid=${apiKey}&units=metric`;
-    axios.get(apiUrl).then(handleSubmit);
+    search();
 
     return "Loading...";
   }
---
+++ src/WeatherInfo.js
@@ -0,0 +1,101 @@
+import React from "react";
+
+import FormattedDate from "./FormattedDate";
+
+export default function WeatherInfo(props) {
+  return (
+    <div className="WeatherInfo">
+      <section className="actual-location">
+        <div className="row">
+          <div className="col-12">
+            <h1>
+              <span className="number-degrees">{props.data.temperature}</span>
+              <span className="celcius">
+                <a href="/" class="active">
+                  °C
+                </a>
+              </span>
+              <span className="fahrenheit">
+                <a href="/" className="active">
+                  °F
+                </a>
+              </span>
+              <span className="actual-weather">🌥️</span>
+            </h1>
+          </div>
+          <div className="col"></div>
+        </div>
+        <div className="row">
+          <span className="city">{props.data.city}</span>
+          <span className="current-location">
+            <button>🧭</button>
+          </span>
+        </div>
+        <div className="row">
+          <h2 id="date">
+            <FormattedDate date={props.data.date} />
+          </h2>
+        </div>
+
+        <ul>
+          <li>
+            <span className="hum">Humidity: </span>
+            <span className="humid">{props.data.humidity}</span>
+            <span className="percentage">%</span>
+          </li>
+          <li>
+            <span className="wind-speed">Wind: </span>
+            <span className="wind">{props.data.wind}</span>
+            <span className="km"> Km/h</span>
+          </li>
+          <li className="text-capitalize">
+            <span className="weather-description">
+              {props.data.description}
+            </span>
+          </li>
+        </ul>
+      </section>
+
+      <section className="next-days">
+        <span className="weather-5days">
+          <div className="row">
+            <div className="col-2">🌥️</div>
+            <div className="col-2">🌥️</div>
+            <div className="col-2">🌥️</div>
+            <div className="col-2">🌥️</div>
+            <div className="col-2">🌦️</div>
+            <div className="col-2">🌦️</div>
+          </div>
+        </span>
+
+        <span className="week-days">
+          <div className="row">
+            <div className="col-2">Sunday</div>
+            <div className="col-2">Monday</div>
+            <div className="col-2">Tuesday</div>
+            <div className="col-2">Wednesday</div>
+            <div className="col-2">Thursday</div>
+            <div className="col-2">Friday</div>
+          </div>
+        </span>
+
+        <span className="daily-temperatures">
+          <div className="row">
+            <div className="col-1">26°</div>
+            <div className="col-1">21°</div>
+            <div className="col-1">24°</div>
+            <div className="col-1">20°</div>
+            <div className="col-1">24°</div>
+            <div className="col-1">20°</div>
+            <div className="col-1">23°</div>
+            <div className="col-1">20°</div>
+            <div className="col-1">25°</div>
+            <div className="col-1">20°</div>
+            <div className="col-1">20°</div>
+            <div className="col-1">24°</div>
+          </div>
+        </span>
+      </section>
+    </div>
+  );
+}
